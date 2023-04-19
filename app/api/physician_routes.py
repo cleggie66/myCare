2dbd5583b8ebda6ad88bdeab3248ceecb0683b2d
Caleb Cleghorn <caleb@cleghorn.org>
2023-04-19 13:39:43 -0500

    added working routes for create, read, and update

--- app/api/physician_routes.py
+++ app/api/physician_routes.py
@@ -46,8 +46,52 @@ def get_physician(physician_id):
 
 @physician_routes.route("", methods=["POST"])
 @login_required
-def create_physician(physician_data):
+def create_physician():
+    form = PhysicianForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        new_physician = Physician(
+            first_name=form.data["first_name"],
+            last_name=form.data["last_name"],
+            picture=form.data["picture"],
+            hospital_id=form.data["hospital_id"],
+            medical_speciality_id=form.data["medical_speciality_id"],
+            medical_education=form.data["medical_education"],
+            accepts_insurance=form.data["accepts_insurance"]
+        )
+        db.session.add(new_physician)
+        db.session.commit()
+        return new_physician.to_dict()
+    return {"Message": "Invalid Data"}
+
+
+# -----------  PUT  --------------
+# Updates a physician
+
+@physician_routes.route("/<physician_id>", methods=["PUT"])
+@login_required
+def update_physician(physician_id):
+    physician = Physician.query.get(physician_id)
+
+    if not physician:
+        return {
+            "message": "Physician could not be found",
+            "status_code": 404
+        }, 404
+    
     form = PhysicianForm()
     form['csrf_token'].data = request.cookies['csrf_token']
 
+    # TODO: Look into refactoring this code:
+    if form.validate_on_submit():
+        physician.first_name=form.data["first_name"]
+        physician.last_name=form.data["last_name"]
+        physician.picture=form.data["picture"]
+        physician.hospital_id=form.data["hospital_id"]
+        physician.medical_speciality_id=form.data["medical_speciality_id"]
+        physician.medical_education=form.data["medical_education"]
+        physician.accepts_insurance=form.data["accepts_insurance"]
 
+        db.session.commit()
+        return physician.to_dict()
+    return {"Message": "Invalid Data"}
