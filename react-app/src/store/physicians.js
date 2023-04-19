4c895013471ab3d21b1c6c603d583a31eacde1b8
Caleb Cleghorn <caleb@cleghorn.org>
2023-04-19 15:50:03 -0500

    started on physicians state

---
+++ react-app/src/store/physicians.js
@@ -0,0 +1,11 @@
+const SET_ALL_PHYSICIANS = 'physicians/set_all_physicians'
+const SET_SINGLE_PHYSICIAN = 'physicians/set_single_physician'
+const CREATE_PHYSICIAN = "physicians/create_physician"
+const UPDATE_PHYSICIAN = "physicians/update_physician"
+const DELETE_PHYSICIAN = "physicians/delete_physician"
+
+// ACTIONS
+
+// THUNKS
+
+// REDUCER
