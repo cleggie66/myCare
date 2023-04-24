b520a4bf1964b96f6ace893ea3702a6cf3bb372d
Caleb Cleghorn <caleb@cleghorn.org>
2023-04-24 17:55:26 -0500

    css

--- react-app/src/components/Appointments/AppointmentForm/CreateAppointmentForm.js
+++ react-app/src/components/Appointments/AppointmentForm/CreateAppointmentForm.js
@@ -8,8 +8,8 @@ const CreateAppointmentForm = () => {
         physicianId: 0,
         hospitalId: 0,
         reasonForVisit: "",
-        startTime: "2019-05-15 17:08:11",
-        endTime: "2019-05-15 17:08:11",
+        startTime: "",
+        endTime: "",
     }
 
     return (
