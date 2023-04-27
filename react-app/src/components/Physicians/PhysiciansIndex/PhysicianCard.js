38ccaaeb5b3f284e4d71eb970a6628c992e9c1b7
Caleb Cleghorn <caleb@cleghorn.org>
2023-04-27 16:31:52 -0500

    physician card css

--- react-app/src/components/Physicians/PhysiciansIndex/PhysicianCard.js
+++ react-app/src/components/Physicians/PhysiciansIndex/PhysicianCard.js
@@ -7,42 +7,58 @@ import "./PhysiciansIndex.css"
 
 const PhysicianCard = ({ physician }) => {
     const history = useHistory();
-    const [iconVisibility, setIconVisibility] = useState("hidden-icons")
+    const [iconVisibility, setIconVisibility] = useState("hidden-physician-icons")
 
+    const playVideo = (video) => {
+
+    }
 
     return (
         <div
             className="physician-card"
             key={physician.id}
             onMouseOver={() => setIconVisibility("")}
-            onMouseLeave={() => setIconVisibility("hidden-icons")}
+            onMouseLeave={() => setIconVisibility("hidden-physician-icons")}
         >
-            <div className="physician-card-content">
+            <div className="physician-card-header">
                 <div className="image-container">
                     <img src={physician.picture} alt="doctor" className="physician-profile-pic" />
                 </div>
-                <a href={physician.video}>Meet the Doctor</a>
-                <h2>{`${physician.first_name} ${physician.last_name} ${physician.medical_education}`}</h2>
-                <h3>{physician.medical_specialty.name}</h3>
-                <h3>{physician.hospital.name}</h3>
-                <div className="physician-card-buttons">
-                    <button>Add to MyCare team</button>
-                    <button
-                    onClick={() => history.push(`/appointment/new/${physician.id}`)}
-                    >Book an Appointment</button>
-                </div>
-            </div>
-            <div className="physician-card-icons">
-                <div className={`physician-card-icon ${iconVisibility}`}>
-                    <i class="fa-solid fa-pen-to-square"
-                        onClick={() => history.push(`/physician/${physician.id}/update`)}
+                <div className="physician-card-icons">
+                    <div className={`physician-card-icon ${iconVisibility}`}>
+                        <i class="fa-solid fa-pen-to-square"
+                            onClick={() => history.push(`/physician/${physician.id}/update`)}
+                        />
+                    </div>
+                    <OpenModalButton
+                        buttonText={<i class="fa-solid fa-trash-can"></i>}
+                        modalComponent={<DeletePhysicianModal physician={physician} />}
+                        className={`physician-card-icon ${iconVisibility}`}
                     />
                 </div>
-                <OpenModalButton
-                    buttonText={<i class="fa-solid fa-trash-can"></i>}
-                    modalComponent={<DeletePhysicianModal physician={physician} />}
-                    className={`physician-card-icon ${iconVisibility}`}
-                />
+            </div>
+            <div className="physician-card-content">
+                <div
+                    onClick={playVideo(physician.video)}
+                    className="physician-card-video"
+                >
+                    <i className="fa-solid fa-circle-play"></i>
+                    <p>Meet the Doctor</p>
+                </div>
+                <h2>{`${physician.first_name} ${physician.last_name} ${physician.medical_education}`}</h2>
+                <h3>{physician.medical_specialty.name}</h3>
+                <h4>{physician.hospital.name}</h4>
+                <div className="physician-card-buttons">
+                    <i className="fa-solid fa-heart"></i>
+                    <button>Add to MyCare team</button>
+                </div>
+                <div
+                    className="physician-card-buttons"
+                    onClick={() => history.push(`/appointment/new/${physician.id}`)}
+                >
+                    <i className="fa-regular fa-calendar-check"></i>
+                    <button>Book an Appointment</button>
+                </div>
             </div>
         </div>
     )
